afee12f5431b63951476bf047d1bf23bca22bd05
vi-ii-mmvii <nur.adina.muratkyzy@gmail.com>
2025-06-07 22:59:50 +0500

    Add files via upload

---
+++ my-react-app/src/videos.js
@@ -0,0 +1,9 @@
+const Videos = [
+    {id: 1, title: "karandash", chanel_name: "aboba3123", likes: 5},
+    {id: 2, title: "karandas123h", likes: 10},
+    {id: 3, title: "karandash123", chanel_name: "aboba123", likes: 9},
+    {id: 4, title: "karandash123", chanel_name: "aboba21321", likes: 10},
+    {id: 5, title: "karandas123h", chanel_name: "abob123123a", likes: 11},
+  ];
+
+export default Videos;
